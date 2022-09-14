11d053dd56382a65860e9897e6c8163801aa600f
suparansharma <suparansharmap@gmail.com>
2022-09-14 20:49:24 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import { initializeApp } from 'firebase/app';
-import firebaseConfig from './firebase.config';
-import { GoogleAuthProvider, getAuth, signInWithPopup,signOut } from 'firebase/auth';
-import { useState } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "./firebase.config";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signOut,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { useState } from "react";
 function App() {
   const [user, setUser] = useState({
     isSignedIn: false,
-    name: '',
-    email: '',
-    photo: '',
+    name: "",
+    email: "",
+    password: "",
+    photo: "",
   });
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app);
@@ -38,49 +46,71 @@ function App() {
         const credential = GoogleAuthProvider.credentialFromError(error);
         console.log(errorMessage);
       });
-
   };
 
   const googleSignOut = () => {
-    signOut(auth)
-    .then((result) => {
+    signOut(auth).then((result) => {
       // const { displayName, email, photoURL } = result.user;
       const signedOutUser = {
         isSignedIn: false,
-        name: '',
-        email: '',
-        photo: '',
+        name: "",
+        email: "",
+        photo: "",
       };
       setUser(signedOutUser);
     });
   };
 
+  const handleBlur = (event) => {
+ 
+    let isFieldValid = true;
 
-  const handleBlur =(event) =>{
-  console.log(event.target.name,event.target.value);
-  if(event.target.name==='email'){
-    const isEmailValid = /\S+@\S+\.\S+/.test(event.target.value);
-    console.log(isEmailValid);
-  }
+    if (event.target.name === "email") {
+      isFieldValid = /\S+@\S+\.\S+/.test(event.target.value);
+   
+    }
 
-  if(event.target.name==='password'){
-    const isPasswordValid = event.target.value.length>0;
-    const isEmailValid = /\d{1}/.test(event.target.value);
-    console.log(isPasswordValid,isEmailValid);
-  }
-  }
+    if (event.target.name === "password") {
+      const isPasswordValid = event.target.value.length > 0;
+      const passwordHasNumber = /\d{1}/.test(event.target.value);
+      isFieldValid = isPasswordValid && passwordHasNumber;
+ 
+    }
 
-  const handleSubmit = () =>{
+    if (isFieldValid) {
+      const newUserInfo = { ...user };
+      newUserInfo[event.target.name] = event.target.value;
+      setUser(newUserInfo);
+    }
+  };
 
-  }
+  const handleSubmit = (event) => {
+    if (user.email && user.password) {
+    
+      const auth = getAuth();
+      console.log(user.email, user.password, "subbmit");
+      createUserWithEmailAndPassword(auth, user.email, user.password)
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorCode, errorMessage);
+        });
+    }
+    event.preventDefault();
+  };
 
   return (
     <div className="App">
-      {user.isSignedIn ? 
+      {user.isSignedIn ? (
         <button onClick={googleSignOut}>Sign Out</button>
-       : 
+      ) : (
         <button onClick={googleSignIn}>Sign In</button>
-      }
+      )}
       {user.isSignedIn && (
         <div>
           <p>Welcome, {user.name}</p>
@@ -89,15 +119,31 @@ function App() {
         </div>
       )}
 
-
-    <h1>Our Own Authentication</h1>
-    <form onSubmit={handleSubmit}>
-      <input type="text" onBlur={handleBlur} name="email" id="" placeholder='Your Email addresss' required /><br />
-      <input type="password" onChange={handleBlur} name="password" id="" placeholder='Your Password' required /><br />
-      <input type="submit" value="submit" />
-    </form>
-
-
+      <h1>Our Own Authentication</h1>
+      <p>Email:{user.email}</p>
+      <p>Password:{user.password}</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onBlur={handleBlur} placeholder="Your Name" /> <br />
+        <input
+          type="text"
+          onBlur={handleBlur}
+          name="email"
+          id=""
+          placeholder="Your Email addresss"
+          required
+        />
+        <br />
+        <input
+          type="password"
+          onChange={handleBlur}
+          name="password"
+          id=""
+          placeholder="Your Password"
+          required
+        />
+        <br />
+        <input type="submit" value="submit" />
+      </form>
     </div>
   );
 }
